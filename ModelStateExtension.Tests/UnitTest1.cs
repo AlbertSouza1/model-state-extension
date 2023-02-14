20c13962bafe291e20709e452c3ae8dea98a565b
Albert Souza <albert.souza@inovafarma.com.br>
2023-02-14 20:51:11 -0300

    feat: create project solution

---
+++ ModelStateExtension.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelStateExtensionTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
